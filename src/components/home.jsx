669370c14438510b36b25d87558f95f1b2ebfc72
Olakunle Salami <kunsal2003@gmail.com>
2020-04-23 17:37:38 +0100

    (feature) add featured projects section to homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
     return (
       <React.Fragment>
         <Jumbotron />
-        <div className="container">
+        <div className="">
           <div className="row">
             <div className="col text-center">
               <h2 className="title">ABOUT</h2>
@@ -18,13 +18,32 @@ class Home extends Component {
           <div className="row py-3">
             <div className="col-md-2"></div>
             <div className="col-md-8 col-md-offset-3">
-              <h5 className="lay-emphasis text-justify">
+              <p className="lay-emphasis text-justify">
                 It is a long established fact that a reader will be distracted
                 by the readable content of a page when looking at its layout.
                 The point of using Lorem Ipsum is that it has a more-or-less
                 normal distribution of letters, as opposed to using 'Content
                 here, content here', making it look like readable English.
-              </h5>
+              </p>
+            </div>
+          </div>
+          <div className="background py-5">
+            <div className="row">
+              <div className="col text-center">
+                <h2 className="title">Featured Projects</h2>
+              </div>
+            </div>
+            <div className="row py-3">
+              <div className="col-md-2"></div>
+              <div className="col-md-8 col-md-offset-3">
+                <p className="lay-emphasis text-justify">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
+              </p>
+              </div>
             </div>
           </div>
         </div>
