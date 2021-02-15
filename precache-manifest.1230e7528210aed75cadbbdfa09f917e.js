5302270f58e1b0e7391af50ce5959f1827051dab
Olakunle Salami <kunsal2003@gmail.com>
2021-02-15 17:24:55 +0100

    Updates

---
+++ precache-manifest.1230e7528210aed75cadbbdfa09f917e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42abed9d838fc0b9a964c49d1847e88",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "24b50d5167c77e6ef106",
+    "url": "/portfolio/static/css/main.e7a425e7.chunk.css"
+  },
+  {
+    "revision": "3317de616c41dd3f9d7a",
+    "url": "/portfolio/static/js/2.94f4e732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.94f4e732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b50d5167c77e6ef106",
+    "url": "/portfolio/static/js/main.36fe5ef9.chunk.js"
+  },
+  {
+    "revision": "57e6766b1c659ed752e7",
+    "url": "/portfolio/static/js/runtime-main.9b45727d.js"
+  },
+  {
+    "revision": "05a25aab39f956013cf1f684b59d7642",
+    "url": "/portfolio/static/media/kunle2.05a25aab.jpg"
+  },
+  {
+    "revision": "bec4677b67d5ba55ea5a744ad0466011",
+    "url": "/portfolio/static/media/macbook-code.bec4677b.jpg"
+  }
+]);
