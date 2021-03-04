9fc1bfc96ca67d17d79cd447dba1c4523d33becd
Olakunle Salami <kunsal2003@gmail.com>
2021-03-04 11:16:34 +0100

    Updates

---
+++ precache-manifest.67a20fdc88b5bdf6233fe47ab84b0176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ec47a7fe9d1c4ecc21998148b59d84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e57456581c9e837e33",
+    "url": "/static/css/main.f556d813.chunk.css"
+  },
+  {
+    "revision": "79ff5cb7ae44f88861e8",
+    "url": "/static/js/2.5940f314.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5940f314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e57456581c9e837e33",
+    "url": "/static/js/main.fc22e62e.chunk.js"
+  },
+  {
+    "revision": "95d24a2df738fec2005e",
+    "url": "/static/js/runtime-main.dc977e61.js"
+  },
+  {
+    "revision": "05a25aab39f956013cf1f684b59d7642",
+    "url": "/static/media/kunle2.05a25aab.jpg"
+  },
+  {
+    "revision": "bec4677b67d5ba55ea5a744ad0466011",
+    "url": "/static/media/macbook-code.bec4677b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1230e7528210aed75cadbbdfa09f917e.js"
+  "/precache-manifest.67a20fdc88b5bdf6233fe47ab84b0176.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
